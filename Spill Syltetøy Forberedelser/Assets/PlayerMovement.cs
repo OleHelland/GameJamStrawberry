1aa75dc94a5922597a8e6cf03b7c7491c520db8f
Ole Helland <ole.helland@gmail.com>
2018-01-23 11:01:42 +0100

    Lagt inn prosjektet

---
+++ Spill Syltetøy Forberedelser/Assets/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speedX = 2f;
+	public float jumpForce = 350f;
+    private Rigidbody2D rb2d;
+
+    public bool grounded;
+    public Transform groundcheck;
+    private float groundRadius = 0.25f;
+    public LayerMask whatIsGround;
+
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Use this for initialization
+    void FixedUpdate () {
+        grounded = Physics2D.OverlapCircle(groundcheck.position, groundRadius, whatIsGround);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Beveger spilleren venstre og høyre
+        var x = Input.GetAxis ("Horizontal") * Time.deltaTime * speedX;
+        transform.Translate(x, 0, 0);
+
+        // Hopp
+        if (Input.GetButtonDown("Jump") && grounded) {
+            rb2d.AddForce(Vector2.up * jumpForce);
+            grounded = false;
+        }
+	}
+}
